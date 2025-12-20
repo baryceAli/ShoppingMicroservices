2d59dc3bce4b894ab582901f92b5ea4cbdd5ebc6
baryceAli <baryce@gmail.com>
2025-12-20 22:29:15 +0300

    Configure sqlserver db

--- Services/CouponAPI/Program.cs
+++ Services/CouponAPI/Program.cs
@@ -4,7 +4,8 @@ using ShoppingMicroservices.Services.CouponAPI.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<AppDbContext>(options => options.UseInMemoryDatabase("CouponInMem"));
+// builder.Services.AddDbContext<AppDbContext>(options => options.UseInMemoryDatabase("CouponInMem"));
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 
 builder.Services.AddControllers();
