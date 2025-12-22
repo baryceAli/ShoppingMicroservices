a0e79bb08a8efcca71662a2b9ca26a782742397e
baryceAli <baryce@gmail.com>
2025-12-22 05:08:38 +0300

    Implement CouponAPI CRUD

--- FrontEnd/web/Controllers/CouponController.cs
+++ FrontEnd/web/Controllers/CouponController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Text.Json;
 using Microsoft.AspNetCore.Mvc;
+using ShoppingMicroservices.FrontEnd.Mapper;
 using ShoppingMicroservices.FrontEnd.Web.Models.Dtos;
 using ShoppingMicroservices.FrontEnd.Web.Service;
 using ShoppingMicroservices.FrontEnd.Web.Service.IService;
@@ -28,12 +29,61 @@ namespace ShoppingMicroservices.FrontEnd.Web.Controllers
             List<CouponDto> coupons = new();
             if (response != null && response.isSuccess)
             {
-                var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true }
-                ;
+                var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
                 coupons = JsonSerializer.Deserialize<List<CouponDto>>(Convert.ToString(response!.Data!), options);
             }
-
             return View(coupons);
+        }
+
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CouponDto couponDto)
+        {
+            if (ModelState.IsValid)
+            {
+                ResponseDto? response = await _couponService.CreateCouponAsync(CouponMapper.MapCouponDtoToAddCouponDto(couponDto));
+                if (response != null && response.isSuccess)
+                {
+                    return RedirectToAction(nameof(Index));
+
+                }
+            }
+            return View(couponDto);
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            ResponseDto? response = await _couponService.GetCouponByIdAsync(id);
+            // CouponDto couponDto = new CouponDto();
+            if (response != null && response.isSuccess)
+            {
+                var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+                var couponDto = JsonSerializer.Deserialize<CouponDto>(Convert.ToString(response!.Data!), options);
+                return View(couponDto);
+            }
+
+            return NotFound();
+            // return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(CouponDto couponDto)
+        {
+            ResponseDto? response = await _couponService.DeleteCouponAsync(couponDto.CouponId);
+            // CouponDto couponDto = new CouponDto();
+            if (response != null && response.isSuccess)
+            {
+                // var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+                // var couponDto = JsonSerializer.Deserialize<CouponDto>(Convert.ToString(response!.Data!), options);
+                return RedirectToAction(nameof(Index));
+
+            }
+
+            return View(couponDto);
         }
 
     }
---
+++ FrontEnd/web/Mapper/CouponMapper.cs
@@ -0,0 +1,18 @@
+
+
+using Riok.Mapperly.Abstractions;
+using ShoppingMicroservices.FrontEnd.Web.Models.Dtos;
+
+namespace ShoppingMicroservices.FrontEnd.Mapper
+{
+    // Enums of source and target have different numeric values -> use ByName strategy to map them
+    [Mapper(EnumMappingStrategy = EnumMappingStrategy.ByName)]
+    public static partial class CouponMapper
+    {
+
+        // [MapProperty(nameof(coupon.CouponId), nameof(CouponDto.CouponId))]
+        public static partial AddCouponDto MapCouponDtoToAddCouponDto(CouponDto couponDto);
+
+
+    }
+}
